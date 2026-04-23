93494c066660d38e769ef81c55254f46373ab871
SebastianBolivar01 <juan.bolivarc@campusucc.edu.co>
2026-04-23 10:28:43 -0500

    Fix: Configurada base de datos SQLite para entorno Vercel (/tmp/db.sqlite3)

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3' if not config('VERCEL', default=False, cast=bool) else '/tmp/db.sqlite3',
     }
 }
 
